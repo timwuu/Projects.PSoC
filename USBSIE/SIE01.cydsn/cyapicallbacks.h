0cbbf0c18d58eb104ce4006cec38f9799c11a53f
timwuu <timwuu@gmail.com>
2016-11-26 08:38:50 +0800

    Init Commit

---
+++ USBSIE/SIE01.cydsn/cyapicallbacks.h
@@ -0,0 +1,24 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef CYAPICALLBACKS_H
+#define CYAPICALLBACKS_H
+    
+    /*Define your macro callbacks here */
+    /*For more information, refer to the Macro Callbacks topic in the PSoC Creator Help.*/
+    #define isr_1_INTERRUPT_INTERRUPT_CALLBACK
+    #define isr_2_INTERRUPT_INTERRUPT_CALLBACK
+    
+    void isr_1_Interrupt_InterruptCallback(void);
+    void isr_2_Interrupt_InterruptCallback(void);
+    
+#endif /* CYAPICALLBACKS_H */   
+/* [] */
